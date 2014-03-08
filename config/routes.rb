0554273d492fbe1de24500ec56c2adc9cc92eb1a
Sanjeev Mishra <sanjeev_mishra@mckinsey.com>
2014-03-08 15:52:22 +0530

    added bootstarp theme and events management

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Talkdog::Application.routes.draw do
+  root 'events#index'
+  resources :events
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
