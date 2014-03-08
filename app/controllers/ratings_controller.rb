9079ccfb64e128c22bbf4f095f9af16ca11a4bf2
Sanjeev Mishra <sanjeev_mishra@mckinsey.com>
2014-03-08 23:21:30 +0530

    working on ratings

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,8 +1,6 @@
 class RatingsController < ApplicationController
   before_action :set_rating, only: [:show, :edit, :update, :destroy]
 
-  # GET /ratings
-  # GET /ratings.json
   def index
     @ratings = Rating.all
   end
