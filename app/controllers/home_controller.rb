7a5934c5ead33b5d6d4c49b2733fefc12e330d13
Sanjeev Mishra <sanjeev_mishra@mckinsey.com>
2014-03-08 22:06:48 +0530

    added the balls to rate the talk

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   def index
     @event = Event.first
-    @talks = @event.talks.where('start_at >= ? or end_at >= ?', Time.now, Time.now)
+    @talks = @event.talks.where('start_at >= ? or end_at >= ?', Time.now, Time.now) if @event
   end
 
 end
