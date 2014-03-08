9baf2e7dc5cab90c6d60e69fbc603179e4e9ef24
Sanjeev Mishra <sanjeev_mishra@mckinsey.com>
2014-03-08 15:10:51 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Talkdog::Application.config.secret_key_base = '8f5716e3bd9f728e378d184ca7ce604470d37776a690f24b5bc6c881a4b31a8fd1dda9b35675ed5b88042158f9eed1c80ade0bc87b3133da825bd2dd9f77e350'
