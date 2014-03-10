aa36f5065404d28d88db09ccf7e461da0e853105
Sanjeev Mishra <sanjeev_mishra@mckinsey.com>
2014-03-10 16:38:03 +0530

    fixed the icons and timeline

--- app/helpers/talks_helper.rb
+++ app/helpers/talks_helper.rb
@@ -1,2 +1,7 @@
 module TalksHelper
+  def timeline_class(talk)
+    return 'success' if Time.now.between?(talk.start_at, talk.end_at)
+    return 'danger' if Time.now >= talk.end_at
+    'warning' if Time.now <= talk.start_at
+  end
 end
