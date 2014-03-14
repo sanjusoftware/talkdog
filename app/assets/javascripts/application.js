e4ca135177983b73c95a1ef77c9f017b08057287
Sanjeev Mishra <sanjeev_mishra@mckinsey.com>
2014-03-14 11:52:15 +0530

    converted the JS to coffee for the rating functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,31 +15,3 @@
 //= require turbolinks
 //= require twitter/bootstrap
 //= require_tree .
-
-
-$(document).ready(function () {
-    $(".rating").on("click", function () {
-        var url = $(this).attr('href');
-
-        $.post(url, function (response) {
-            if (response == "error") {
-                $("#rating_message").html("<div class='alert alert-danger'><strong>Oh snap!<strong> try submitting again</div>");
-            }
-            else {
-                if(response > 0) {
-                    $("#rating_message").html("<div class='alert alert-success'><strong>Well done!<strong></strong> It's mesmerizing</div>");
-                    $("#rating_progress").append("<div class='bar bar-success' style='width: 15%'>Great</div>");
-                }
-                else if (response < 0) {
-                    $("#rating_progress").append("<div class='bar bar-danger' style='width: 15%'>WTF</div>");
-                    $("#rating_message").html("<div class='alert alert-warning'><strong>Oh Crap!</strong> It's getting bored</div>");
-                }
-                else {
-                    $("#rating_message").html("<div class='alert alert-info'><strong>Poor!</strong> It's not so interesting yet</div>");
-                    $("#rating_progress").append("<div class='bar bar-warning' style='width: 15%'>Neutral</div>");
-                }
-            }
-        });
-        return false;
-    });
-});
--- app/assets/javascripts/talks.js.coffee
+++ app/assets/javascripts/talks.js.coffee
@@ -1,3 +1,25 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(document).ready ->
+  $(".rating").on "click", ->
+    url = $(this).attr("href")
+    $.post url, (response) ->
+      if response is "error"
+        $("#rating_message").html "<div class='alert alert-danger'><strong>Oh snap!<strong> try submitting again</div>"
+      else
+        if response > 0
+          $("#rating_message").html "<div class='alert alert-success'><strong>Well done!<strong></strong> It's mesmerizing</div>"
+          $("#rating_progress").append "<div class='bar bar-success' style='width: 15%'>Great</div>"
+        else if response < 0
+          $("#rating_progress").append "<div class='bar bar-danger' style='width: 15%'>WTF</div>"
+          $("#rating_message").html "<div class='alert alert-warning'><strong>Oh Crap!</strong> It's getting bored</div>"
+        else
+          $("#rating_message").html "<div class='alert alert-info'><strong>Poor!</strong> It's not so interesting yet</div>"
+          $("#rating_progress").append "<div class='bar bar-warning' style='width: 15%'>Neutral</div>"
+      return
+
+    false
+
+  return
